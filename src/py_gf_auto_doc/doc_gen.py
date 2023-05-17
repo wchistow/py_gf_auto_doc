c0d6428c838c88ddbe8bad15eda0e39c0b947afe
Vladimir Chistov <wchistow@yandex.ru>
2023-05-17 12:43:04 +0300

    добавил функцию `doc_gen.get_prog_elems` (только для функций)

--- src/py_gf_auto_doc/doc_gen.py
+++ src/py_gf_auto_doc/doc_gen.py
@@ -1,3 +1,4 @@
+import ast
 import os
 
 
@@ -16,4 +17,30 @@ def get_py_files(path: str) -> list[str | list]:
             inner_result = get_py_files(item)
             if inner_result:  # Если во вложенной папке есть файлы `.py`.
                 result.append(inner_result)
+    return result
+
+
+def get_prog_elems(file: str) -> list[tuple[str, str, str | None]]:
+    body = ast.parse(open(file, encoding='utf-8').read()).body
+    result = []
+    for elem in body:
+        match elem:
+            case ast.FunctionDef(name=name, args=args):
+                pos_only_args = [f'{arg.arg}{(": " + arg.annotation.id) if arg.annotation else ""}'  # type: ignore[attr-defined]
+                                 for arg in args.posonlyargs]
+                other_args = [f'{arg.arg}{(": " + arg.annotation.id) if arg.annotation else ""}'  # type: ignore[attr-defined]
+                              for arg in args.args]
+                kw_only_args = [f'{arg.arg}{(": " + arg.annotation.id) if arg.annotation else ""}'  # type: ignore[attr-defined]
+                                for arg in args.kwonlyargs]
+                result.append(
+                    (
+                        'func',
+                        f'{name}({"*, ".join(pos_only_args)}{", " if pos_only_args else ""}' +
+                        f'{", ".join(other_args)}{", " if kw_only_args else ""}' +
+                        f'{"/, " if kw_only_args else ""}{", ".join(kw_only_args)})' +
+                        f'{(" -> " + ast.unparse(elem.returns)) if elem.returns else ""}',  # type: ignore[attr-defined]
+                        ast.get_docstring(elem)
+                    )
+                )
+
     return result
