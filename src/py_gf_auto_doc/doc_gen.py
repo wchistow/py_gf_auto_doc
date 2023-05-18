e174de2131e88972b9256427f11884440bd8f51e
Vladimir Chistov <wchistow@yandex.ru>
2023-05-18 10:13:36 +0300

    рефакторинг в модуле `doc_gen`: добавил строк документации, отделил обработку функций

--- src/py_gf_auto_doc/doc_gen.py
+++ src/py_gf_auto_doc/doc_gen.py
@@ -1,8 +1,10 @@
+"""В этом модуле находится код генерирования документации."""
 import ast
 import os
 
 
 def generate_doc(path: str):
+    """Главная функция генерирования документации."""
     files = get_py_files(path)
     print(files)
 
@@ -21,26 +23,36 @@ def get_py_files(path: str) -> list[str | list]:
 
 
 def get_prog_elems(file: str) -> list[tuple[str, str, str | None]]:
+    """Возвращает все элементы программы (функции, классы, методы)."""
     body = ast.parse(open(file, encoding='utf-8').read()).body
     result = []
     for elem in body:
         match elem:
-            case ast.FunctionDef(name=name, args=args):
-                pos_only_args = [f'{arg.arg}{(": " + arg.annotation.id) if arg.annotation else ""}'  # type: ignore[attr-defined]
-                                 for arg in args.posonlyargs]
-                other_args = [f'{arg.arg}{(": " + arg.annotation.id) if arg.annotation else ""}'  # type: ignore[attr-defined]
-                              for arg in args.args]
-                kw_only_args = [f'{arg.arg}{(": " + arg.annotation.id) if arg.annotation else ""}'  # type: ignore[attr-defined]
-                                for arg in args.kwonlyargs]
-                result.append(
-                    (
-                        'func',
-                        f'{name}({", ".join(pos_only_args)}{"/, " if pos_only_args else ""}' +
-                        f'{", ".join(other_args)}{", " if kw_only_args else ""}' +
-                        f'{"*, " if kw_only_args else ""}{", ".join(kw_only_args)})' +
-                        f'{(" -> " + ast.unparse(elem.returns)) if elem.returns else ""}',  # type: ignore[attr-defined]
-                        ast.get_docstring(elem)
-                    )
-                )
-
+            case ast.FunctionDef():
+                result.append(_handle_func(elem))
     return result
+
+
+def _handle_func(elem: ast.FunctionDef) -> tuple[str, str, str | None]:
+    """
+    Возвращает кортеж из типа элемента (здесь всегда `'func'`),
+    сигнатуры и строки документации (может быть `None`).
+    """
+    args = elem.args
+    pos_only_args = [f'{arg.arg}' +
+                     f'{(": " + ast.unparse(arg.annotation)) if arg.annotation else ""}'
+                     for arg in args.posonlyargs]
+    other_args = [f'{arg.arg}' +
+                  f'{(": " + ast.unparse(arg.annotation)) if arg.annotation else ""}'
+                  for arg in args.args]
+    kw_only_args = [f'{arg.arg}' +
+                    f'{(": " + ast.unparse(arg.annotation)) if arg.annotation else ""}'
+                    for arg in args.kwonlyargs]
+    return (
+            'func',
+            f'{elem.name}({", ".join(pos_only_args)}{"/, " if pos_only_args else ""}' +
+            f'{", ".join(other_args)}{", " if kw_only_args else ""}' +
+            f'{"*, " if kw_only_args else ""}{", ".join(kw_only_args)})' +
+            f'{(" -> " + ast.unparse(elem.returns)) if elem.returns else ""}',
+            ast.get_docstring(elem)
+           )
