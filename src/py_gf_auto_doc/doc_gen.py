e0506d8e245cd4432921a8453c19319b4da32d1e
Vladimir Chistov <wchistow@yandex.ru>
2023-05-19 14:06:27 +0300

    добавил поддержку классов в функцию `doc_gen.get_prog_elems`

--- src/py_gf_auto_doc/doc_gen.py
+++ src/py_gf_auto_doc/doc_gen.py
@@ -1,6 +1,10 @@
 """В этом модуле находится код генерирования документации."""
 import ast
+from typing import TypeAlias
 import os
+
+FuncT: TypeAlias = tuple[str, str, str | None]
+ClassT: TypeAlias = tuple[str, str, str | None, list[FuncT]]
 
 
 def generate_doc(path: str):
@@ -22,15 +26,29 @@ def get_py_files(path: str) -> list[str | list]:
     return result
 
 
-def get_prog_elems(code: str) -> list[tuple[str, str, str | None]]:
+def get_prog_elems(code: str) -> list[FuncT | ClassT]:
     """Возвращает все элементы программы (функции, классы, методы)."""
     body = ast.parse(code).body
-    result = []
+    result: list[FuncT | ClassT] = []
     for elem in body:
         match elem:
             case ast.FunctionDef():
                 signature = ast.unparse(elem).splitlines()[0][4:-1]
-                if (not signature.startswith('_')) or signature.startswith('__'):
+                if (not signature.startswith('_')) or \
+                        (signature.startswith('__') and signature.endswith('__')):
                     result.append(('func', signature,
                                    ast.get_docstring(elem)))
+            case ast.ClassDef():
+                signature = ast.unparse(elem).splitlines()[0][6:-1]
+                if not signature.startswith('_'):
+                    attrs = []
+                    for node in elem.body:
+                        match node:
+                            case ast.FunctionDef():
+                                meth_signature = ast.unparse(node).splitlines()[0][4:-1]
+                                if (not signature.startswith('_')) or \
+                                        (signature.startswith('__') and signature.endswith('__')):
+                                    attrs.append(('meth', meth_signature, ast.get_docstring(node)))
+
+                    result.append(('class', signature, ast.get_docstring(elem), attrs))
     return result
