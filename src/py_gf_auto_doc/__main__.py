6adb6b0337ed6cf6dbde0f27b4ebb5ae7934aaa8
Vladimir Chistov <wchistow@yandex.ru>
2023-05-25 14:23:21 +0300

    добавил справку

--- src/py_gf_auto_doc/__main__.py
+++ src/py_gf_auto_doc/__main__.py
@@ -4,7 +4,8 @@ import sys
 from .doc_gen import generate_doc
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dir', '-d', type=str)
-parser.add_argument('--out_dir', '-o', type=str)
+parser.add_argument('--dir', '-d', type=str, help='каталог Python-проекта', required=True)
+parser.add_argument('--out_dir', '-o', type=str,
+                    help='каталог для сохранения документации', required=True)
 args = parser.parse_args(sys.argv[1:])
 generate_doc(vars(args)['dir'], vars(args)['out_dir'])
